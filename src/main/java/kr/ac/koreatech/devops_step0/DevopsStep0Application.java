dcdf46fdacfd3d06af5d26ebfebf6b393ad5c98d
gitaidev1004 <gitaidev1004@gmail.com>
2024-12-07 14:57:31 +0900

    first commit

---
+++ src/main/java/kr/ac/koreatech/devops_step0/DevopsStep0Application.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.devops_step0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsStep0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsStep0Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/koreatech/devops_step0/DevopsStep0ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.koreatech.devops_step0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsStep0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
